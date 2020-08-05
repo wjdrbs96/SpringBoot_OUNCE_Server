caeb40375cbb2930a46076cc3a3ebd75efb6a645
최정균 <wjdrbs966@naver.com>
2020-08-06 00:47:09 +0900

    initial commit

---
+++ src/main/java/me/gyun/ounce/OunceApplication.java
@@ -0,0 +1,13 @@
+package me.gyun.ounce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OunceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OunceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gyun/ounce/OunceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gyun.ounce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OunceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
